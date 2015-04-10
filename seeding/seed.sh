64560fdcac6ad55c774817571334a94b742b69c4
Juan Batiz-Benet <juan@benet.ai>
2015-04-09 21:37:19 -0700

    make sure refs are pulled
    this is because there's a bug in pin

--- seeding/seed.sh
+++ seeding/seed.sh
@@ -1,3 +1,5 @@
 #!/bin/sh
+cat refs-to-seed | egrep '^[^#]+$' | xargs echo ipfs refs -r
+cat refs-to-seed | egrep '^[^#]+$' | xargs ipfs refs -r
 cat refs-to-seed | egrep '^[^#]+$' | xargs echo ipfs pin add -r
 cat refs-to-seed | egrep '^[^#]+$' | xargs ipfs pin add -r
