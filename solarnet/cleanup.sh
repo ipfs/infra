fbabc0f606f12be54035345608156fab3d413808
Lars Gierth <larsg@systemli.org>
2015-07-10 12:38:35 +0200

    solarnet: fix cleanup.sh host selection
    License: MIT
    Signed-off-by: Lars Gierth <larsg@systemli.org>

--- solarnet/cleanup.sh
+++ solarnet/cleanup.sh
@@ -1,5 +1,6 @@
 #!/bin/sh
 
-for host in `ansible solarnet --list-hosts`; do
+for host in `ansible all --list-hosts`; do
+  echo "==> $host"
   ssh root@$host.i.ipfs.io 'rm -v /var/lib/docker/containers/*/*-json.log ; docker restart ipfs ; sleep 2 ; docker rm $(docker ps -f "status=exited" -aq) ; docker rmi $(docker images -f "dangling=true" -aq)'
 done
