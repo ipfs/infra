eb387ede2410144dabcd738a203cf2e1717bf2d1
Lars Gierth <larsg@systemli.org>
2015-09-04 16:30:18 +0200

    solarnet: clean up only gateway hosts, and don't restart ipfs
    License: MIT
    Signed-off-by: Lars Gierth <larsg@systemli.org>

--- solarnet/cleanup.sh
+++ solarnet/cleanup.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 
-for host in `ansible all --list-hosts`; do
+for host in `ansible gateway --list-hosts`; do
   echo "==> $host"
-  ssh root@$host.i.ipfs.io 'rm -v /var/lib/docker/containers/*/*-json.log ; docker restart ipfs ; sleep 2 ; docker rm $(docker ps -f "status=exited" -aq) ; docker rmi $(docker images -f "dangling=true" -aq)'
+  ssh root@$host.i.ipfs.io 'rm -v /var/lib/docker/containers/*/*-json.log ; docker rm $(docker ps -f "status=exited" -aq) ; docker rmi $(docker images -f "dangling=true" -aq)'
 done
