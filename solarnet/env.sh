a825bc44c44f2a69b7b30ec168ccd5a30c1d337b
Lars Gierth <larsg@systemli.org>
2016-01-29 00:06:46 +0100

    solarnet: add the production hosts
    License: MIT
    Signed-off-by: Lars Gierth <larsg@systemli.org>

--- solarnet/env.sh
+++ solarnet/env.sh
@@ -1,12 +1,52 @@
 #!/usr/bin/env bash
 
-provsn_hosts=(spacerock)
+provsn_hosts=(spacerock pluto neptune uranus saturn jupiter venus earth mercury pollux biham nihal phobos deimos)
 provsn_groups=()
 
 all_ssh_options="-o ConnectTimeout=5"
 all_omit_build=(secrets/ipfs)
 
 baseunits=(base docker)
+gatewayunits=(ipfs ipfs/multireq secrets/ipfs)
 
 spacerock_ssh="root@spacerock.i.ipfs.team"
-spacerock_units=(${baseunits[@]} ipfs ipfs/multireq secrets/ipfs)
+spacerock_units=(${baseunits[@]} ${gatewayunits[@]})
+
+pluto_ssh="root@pluto.i.ipfs.io"
+pluto_units=(${baseunits[@]} ${gatewayunits[@]})
+
+neptune_ssh="root@neptune.i.ipfs.io"
+neptune_units=(${baseunits[@]} ${gatewayunits[@]})
+
+uranus_ssh="root@uranus.i.ipfs.io"
+uranus_units=(${baseunits[@]} ${gatewayunits[@]})
+
+saturn_ssh="root@saturn.i.ipfs.io"
+saturn_units=(${baseunits[@]} ${gatewayunits[@]})
+
+jupiter_ssh="root@jupiter.i.ipfs.io"
+jupiter_units=(${baseunits[@]} ${gatewayunits[@]})
+
+venus_ssh="root@venus.i.ipfs.io"
+venus_units=(${baseunits[@]} ${gatewayunits[@]})
+
+earth_ssh="root@earth.i.ipfs.io"
+earth_units=(${baseunits[@]} ${gatewayunits[@]})
+
+mercury_ssh="root@mercury.i.ipfs.io"
+mercury_units=(${baseunits[@]} ${gatewayunits[@]})
+
+pollux_ssh="root@pollux.i.ipfs.io"
+pollux_units=(${baseunits[@]})
+
+biham_ssh="root@biham.i.ipfs.io"
+biham_units=(${baseunits[@]})
+
+nihal_ssh="root@nihal.i.ipfs.io"
+nihal_units=(${baseunits[@]})
+
+phobos_ssh="root@phobos.i.ipfs.io"
+phobos_units=(${baseunits[@]})
+
+deimos_ssh="root@deimos.i.ipfs.io"
+deimos_units=(${baseunits[@]})
--- solarnet/ipfs/env.sh
+++ solarnet/ipfs/env.sh
@@ -2,7 +2,8 @@
 
 all_ipfs_git=git://github.com/ipfs/go-ipfs
 all_ipfs_ref=89a6f01be017b4da63492a1a1f76d4607e49c931
-all_ipfs_repo=/mnt/data/ipfs
+# TODO: move to /mnt/data/ipfs
+all_ipfs_repo=/ipfs/ipfs_master/repo
 all_ipfs_swarm_tcp=4001
 all_ipfs_swarm_utp=4002
 all_ipfs_api=5001
@@ -10,7 +11,8 @@ all_ipfs_gateway=8080
 
 all_ipfs_v03x_git=git://github.com/ipfs/go-ipfs
 all_ipfs_v03x_ref=b7d35e3af596bd2241a9633f3c11080f01b9b494
-all_ipfs_v03x_repo=/mnt/data/ipfs_v03x
+# TODO: move to /mnt/data/ipfs_v03x
+all_ipfs_v03x_repo=/ipfs/ipfs_master/repo_v2
 all_ipfs_v03x_api=15001
 all_ipfs_v03x_gateway=18080
 all_ipfs_v03x_swarm_tcp=14001
@@ -19,3 +21,24 @@ all_ipfs_v03x_swarm_utp=14002
 all_ipfs_gc_period=20m
 all_ipfs_gc_watermark=90
 all_ipfs_gc_capacity=10G
+
+earth_ipfs_swarm_tcp=14001
+earth_ipfs_swarm_utp=14002
+earth_ipfs_v03x_swarm_tcp=4001
+earth_ipfs_v03x_swarm_utp=4002
+saturn_ipfs_swarm_tcp=14001
+saturn_ipfs_swarm_utp=14002
+saturn_ipfs_v03x_swarm_tcp=4001
+saturn_ipfs_v03x_swarm_utp=4002
+jupiter_ipfs_swarm_tcp=14001
+jupiter_ipfs_swarm_utp=14002
+jupiter_ipfs_v03x_swarm_tcp=4001
+jupiter_ipfs_v03x_swarm_utp=4002
+venus_ipfs_swarm_tcp=14001
+venus_ipfs_swarm_utp=14002
+venus_ipfs_v03x_swarm_tcp=4001
+venus_ipfs_v03x_swarm_utp=4002
+mercury_ipfs_swarm_tcp=14001
+mercury_ipfs_swarm_utp=14002
+mercury_ipfs_v03x_swarm_tcp=4001
+mercury_ipfs_v03x_swarm_utp=4002
