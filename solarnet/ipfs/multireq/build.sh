611adc181a241f9ddcaaffc6cb8d272c045aca2e
Lars Gierth <larsg@systemli.org>
2016-01-29 01:06:14 +0100

    ipfs: have multireq listen only locally
    License: MIT
    Signed-off-by: Lars Gierth <larsg@systemli.org>

--- solarnet/ipfs/multireq/build.sh
+++ solarnet/ipfs/multireq/build.sh
@@ -8,7 +8,7 @@ cat > docker.opts <<-EOF
 --restart always
 --net host
 multireq:$(lookup multireq_ref)
-:8040
+127.0.0.1:8040
 http://127.0.0.1:$(lookup ipfs_gateway)
 http://127.0.0.1:$(lookup ipfs_v03x_gateway)
 EOF
