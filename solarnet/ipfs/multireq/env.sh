951e44d6659f3775c6a68bdc30ba41aadeccc29b
Lars Gierth <larsg@systemli.org>
2016-01-29 04:43:20 +0100

    multireq: update to lgierth:premature-close
    License: MIT
    Signed-off-by: Lars Gierth <larsg@systemli.org>

--- solarnet/ipfs/multireq/env.sh
+++ solarnet/ipfs/multireq/env.sh
@@ -1,4 +1,4 @@
 #!/usr/bin/env bash
 
 all_multireq_git='git://github.com/lgierth/multireq'
-all_multireq_ref='57061b0118debd83cf5cae704320c47d8e264817'
+all_multireq_ref='fd4fad6c27bf0ff265416b0473884fc1e4ca69a1'
