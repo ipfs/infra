d3e3d30f7cb6bdf2f2464640845ef7cfdc237d7e
Lars Gierth <larsg@systemli.org>
2016-02-17 16:16:11 +0100

    multireq: update to latest master
    License: MIT
    Signed-off-by: Lars Gierth <larsg@systemli.org>

--- solarnet/ipfs/multireq/env.sh
+++ solarnet/ipfs/multireq/env.sh
@@ -1,4 +1,4 @@
 #!/usr/bin/env bash
 
 all_multireq_git='git://github.com/lgierth/multireq'
-all_multireq_ref='fd4fad6c27bf0ff265416b0473884fc1e4ca69a1'
+all_multireq_ref='152e37ce0cdd63b681c61c5c2d9786bdfa8e18f0'
