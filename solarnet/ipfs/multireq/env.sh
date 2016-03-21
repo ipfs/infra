c9d817503ad2332d951e362ea2d69468346b8300
Lars Gierth <larsg@systemli.org>
2016-03-21 12:44:06 -0400

    multireq: fix git repo
    License: MIT
    Signed-off-by: Lars Gierth <larsg@systemli.org>

--- solarnet/ipfs/multireq/env.sh
+++ solarnet/ipfs/multireq/env.sh
@@ -1,4 +1,4 @@
 #!/usr/bin/env bash
 
-all_multireq_git='git://github.com/lgierth/multireq'
+all_multireq_git='git://github.com/whyrusleeping/multireq'
 all_multireq_ref='0c5258d3ac44cd85c1475dac4aeb843193be01f4'
