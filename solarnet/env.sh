e9e566bd783f62f84fbbaa44f34dbe7c5d56e7cd
Lars Gierth <larsg@systemli.org>
2016-03-27 18:00:13 -0400

    solarnet: use IP addresses instead of hostnames
    License: MIT
    Signed-off-by: Lars Gierth <larsg@systemli.org>

--- solarnet/env.sh
+++ solarnet/env.sh
@@ -9,44 +9,46 @@ all_omit_build=(secrets/ipfs)
 baseunits=(base docker)
 gatewayunits=(ipfs ipfs/multireq secrets/ipfs)
 
-spacerock_ssh="root@spacerock.i.ipfs.team"
+spacerock_ssh="root@46.101.117.27"
 spacerock_units=(${baseunits[@]} ${gatewayunits[@]})
+otherrock_ssh="root@46.101.233.94"
+otherrock_units=(${baseunits[@]} ${gatewayunits[@]})
 
-pluto_ssh="root@pluto.i.ipfs.io"
+pluto_ssh="root@104.236.179.241"
 pluto_units=(${baseunits[@]} ${gatewayunits[@]})
 
-neptune_ssh="root@neptune.i.ipfs.io"
+neptune_ssh="root@104.236.176.52"
 neptune_units=(${baseunits[@]} ${gatewayunits[@]})
 
-uranus_ssh="root@uranus.i.ipfs.io"
+uranus_ssh="root@162.243.248.213"
 uranus_units=(${baseunits[@]} ${gatewayunits[@]})
 
-saturn_ssh="root@saturn.i.ipfs.io"
+saturn_ssh="root@128.199.219.111"
 saturn_units=(${baseunits[@]} ${gatewayunits[@]})
 
-jupiter_ssh="root@jupiter.i.ipfs.io"
+jupiter_ssh="root@104.236.151.122"
 jupiter_units=(${baseunits[@]} ${gatewayunits[@]})
 
-venus_ssh="root@venus.i.ipfs.io"
+venus_ssh="root@104.236.76.40"
 venus_units=(${baseunits[@]} ${gatewayunits[@]})
 
-earth_ssh="root@earth.i.ipfs.io"
+earth_ssh="root@178.62.158.247"
 earth_units=(${baseunits[@]} ${gatewayunits[@]})
 
-mercury_ssh="root@mercury.i.ipfs.io"
+mercury_ssh="root@178.62.61.185"
 mercury_units=(${baseunits[@]} ${gatewayunits[@]})
 
-pollux_ssh="root@pollux.i.ipfs.io"
+pollux_ssh="root@5.9.59.34"
 pollux_units=(${baseunits[@]})
 
-biham_ssh="root@biham.i.ipfs.io"
+biham_ssh="root@188.40.114.11"
 biham_units=(${baseunits[@]})
 
-nihal_ssh="root@nihal.i.ipfs.io"
+nihal_ssh="root@188.40.116.66"
 nihal_units=(${baseunits[@]})
 
-phobos_ssh="root@phobos.i.ipfs.io"
+phobos_ssh="root@104.131.3.162"
 phobos_units=(${baseunits[@]})
 
-deimos_ssh="root@deimos.i.ipfs.io"
+deimos_ssh="root@46.101.230.158"
 deimos_units=(${baseunits[@]})
