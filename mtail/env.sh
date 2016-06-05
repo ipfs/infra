51df955dd74bf4ad7bad5f7136c6f1e40c763195
Lars Gierth <larsg@systemli.org>
2016-06-05 21:57:20 +0200

    mtail: update to latest master

--- mtail/env.sh
+++ mtail/env.sh
@@ -1,4 +1,4 @@
 #!/usr/bin/env bash
 
 all_mtail_git='git://github.com/google/mtail'
-all_mtail_ref='827bd744c581137294204a5970115174d6302e76'
+all_mtail_ref='a54f860fe8b14440320014ed608e438a790f19ac'
