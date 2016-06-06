42544e5e2bdc93361b159958c8ac1d626b8e32c9
Lars Gierth <larsg@systemli.org>
2016-06-06 02:47:31 +0200

    hosts: add host for orbit, called rigel (Foot of the Giant)
    License: MIT
    Signed-off-by: Lars Gierth <larsg@systemli.org>

--- env.sh
+++ env.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 
-provsn_hosts=(pluto neptune uranus saturn jupiter venus earth mercury pollux biham nihal phobos deimos)
+provsn_hosts=(pluto neptune uranus saturn jupiter venus earth mercury pollux biham nihal phobos deimos rigel)
 # provsn_hosts=(pluto neptune uranus saturn jupiter venus earth mercury pollux biham nihal)
 # provsn_hosts=(pluto neptune uranus saturn jupiter venus earth mercury)
 provsn_groups=()
@@ -50,3 +50,6 @@ phobos_units=(${baseunits[@]})
 
 deimos_ssh="root@46.101.230.158"
 deimos_units=(${baseunits[@]})
+
+rigel_ssh="root@139.59.133.121"
+rigel_units=(${baseunits[@]})
