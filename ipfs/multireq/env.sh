8f0b79a028415af424c862db92a2e1bae6eacc58
Lars Gierth <larsg@systemli.org>
2016-06-15 15:58:59 +0200

    multireq: deploy 2-month-old fix for HEAD requests :S
    License: MIT
    Signed-off-by: Lars Gierth <larsg@systemli.org>

--- ipfs/multireq/env.sh
+++ ipfs/multireq/env.sh
@@ -1,4 +1,4 @@
 #!/usr/bin/env bash
 
 all_multireq_git='git://github.com/whyrusleeping/multireq'
-all_multireq_ref='0c5258d3ac44cd85c1475dac4aeb843193be01f4'
+all_multireq_ref='9c7bfc8e5f869b5f7d1eaccfd52efa12cfc59444'
