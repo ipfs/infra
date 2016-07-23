6bee5579c555cbd598e4a21e4d0a93bdfadf0cb6
Lars Gierth <larsg@systemli.org>
2016-07-23 15:57:44 +0100

    ipfs: update to v0.4.3-rc1

--- ipfs/env.sh
+++ ipfs/env.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 
 all_ipfs_git=git://github.com/ipfs/go-ipfs
-all_ipfs_ref=994109c7316166fe89e6f4df91b91ca93283691c
+all_ipfs_ref=566c08e256bdbd5a292a1518c5a9a399b99526f3
 # TODO: move to /mnt/data/ipfs
 all_ipfs_repo=/ipfs/ipfs_master/repo
 all_ipfs_swarm_tcp=4001
@@ -9,19 +9,15 @@ all_ipfs_swarm_utp=4002
 all_ipfs_api=5001
 all_ipfs_gateway=8080
 
-pluto_ipfs_ref=bc6d71f4cdea90f61b0c9f2d1a68c6474e4c65b9
-jupiter_ipfs_ref=bc6d71f4cdea90f61b0c9f2d1a68c6474e4c65b9
-saturn_ipfs_ref=bc6d71f4cdea90f61b0c9f2d1a68c6474e4c65b9
-
 # storage hosts, coordinate ipfs deploys with storage users (e.g. @davidar)
 biham_ipfs_ref=f3d5fe6c6518b44d82c5e77cb34e7f6f0168d82d
-pollux_ipfs_ref=bc6d71f4cdea90f61b0c9f2d1a68c6474e4c65b9
+pollux_ipfs_ref=566c08e256bdbd5a292a1518c5a9a399b99526f3
 nihal_ipfs_ref=994109c7316166fe89e6f4df91b91ca93283691c
 
 # canaries (@whyrusleeping)
 # XXX should one of these be a gateway?
-mercury_ipfs_ref=bc6d71f4cdea90f61b0c9f2d1a68c6474e4c65b9
-neptune_ipfs_ref=bc6d71f4cdea90f61b0c9f2d1a68c6474e4c65b9
+mercury_ipfs_ref=566c08e256bdbd5a292a1518c5a9a399b99526f3
+neptune_ipfs_ref=566c08e256bdbd5a292a1518c5a9a399b99526f3
 
 all_ipfs_v03x_git=git://github.com/ipfs/go-ipfs
 all_ipfs_v03x_ref=b21cff6f3efacb5578a2521e2ff27b4c10678c03
