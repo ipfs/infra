1d250417ee72734db625ae29cd3a79e884514991
Lars Gierth <larsg@systemli.org>
2016-10-17 22:55:09 -0400

    docker: migrate daemon config from ansible
    License: MIT
    Signed-off-by: Lars Gierth <larsg@systemli.org>

--- docker/install.sh
+++ docker/install.sh
@@ -3,3 +3,22 @@
 set -e
 
 which docker >/dev/null || ./getdocker.sh
+
+restart=0
+
+target=/opt/docker
+mkdir -p "$target"
+
+if [ ! -z "$(git diff "$target/config" "config" 2>&1 || echo "new")" ]; then
+  echo "docker config changed"
+  restart=1
+fi
+
+cp config /etc/default/docker
+
+if [ "restart$restart" == "restart1" ]; then
+  echo "docker restarting"
+  # TODO systemd
+  restart docker 2>&1 >/dev/null || start docker >/dev/null
+  cp config "$target/config"
+fi
