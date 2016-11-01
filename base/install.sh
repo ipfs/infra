d31d752abdd54a0387cb71d7060a22d94c77377b
Lars Gierth <larsg@systemli.org>
2016-11-01 04:42:31 +0100

    base: use git only after installing it
    License: MIT
    Signed-off-by: Lars Gierth <larsg@systemli.org>

--- base/install.sh
+++ base/install.sh
@@ -2,15 +2,6 @@
 
 set -e
 
-dir="$HOME/.ssh"
-if [ ! -z "$(git diff "$dir/authorized_keys" authorized_keys || echo new)" ]; then
-  echo "$unit authorized_keys changed"
-  mkdir -p "$dir"
-  chmod 700 "$dir"
-  cp authorized_keys "$dir/authorized_keys"
-  chmod 400 "$dir/authorized_keys"
-fi
-
 pkgs=(mosh tmux screen gdb vim tree htop iftop sysstat bridge-utils unzip jq)
 pkgs+=(git mercurial nodejs npm build-essential autoconf libtool bison flex)
 apt-get install -qq -y "${pkgs[@]}"
@@ -24,4 +15,13 @@ if [ "$host" != "$(hostname)" ]; then
   echo "setting hostname"
   hostname "$host"
   echo "$host" > /etc/hostname
+fi
+
+dir="$HOME/.ssh"
+if [ ! -z "$(git diff "$dir/authorized_keys" authorized_keys || echo new)" ]; then
+  echo "$unit authorized_keys changed"
+  mkdir -p "$dir"
+  chmod 700 "$dir"
+  cp authorized_keys "$dir/authorized_keys"
+  chmod 400 "$dir/authorized_keys"
 fi
