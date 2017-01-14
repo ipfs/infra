0258c25c026874ed184362717b371ffbb645aaef
Lars Gierth <larsg@systemli.org>
2017-01-14 04:58:01 +0100

    base: and unit for golang installation
    License: MIT
    Signed-off-by: Lars Gierth <larsg@systemli.org>

---
+++ base/golang/env.sh
@@ -0,0 +1,5 @@
+#!/usr/bin/env bash
+
+all_golang_version=go1.7.4
+all_golang_sha256sum=47fda42e46b4c3ec93fa5d4d4cc6a748aa3f9411a2a2b7e08e3a6d80d753ec8b
+all_golang_goroot=/usr/local/go
---
+++ base/golang/install.sh
@@ -0,0 +1,37 @@
+#!/usr/bin/env bash
+
+set -e
+
+# How this install script works
+#
+# 1. Checks if requested version changed
+# 2. Downloads and installs new version
+# 3. Installs /etc/profile script for $GOPATH et. al.
+
+target="/opt/golang"
+
+reinstall=0
+
+version=$(lookup golang_version)
+tarball="$version.linux-amd64.tar.gz"
+tarball_url="https://storage.googleapis.com/golang/$tarball"
+goroot=$(lookup golang_goroot)
+
+actual_version=$($goroot/bin/go version | cut -d' ' -f3 || true)
+if [ "$version" != "$actual_version" ]; then
+  reinstall=1
+fi
+
+if [ "reinstall$reinstall" == "reinstall1" ]; then
+  wget -N -q "$tarball_url"
+  if ! md5sum "$tarball" | grep "$checksum" > /dev/null; then
+    echo "golang $version download failed or checksum mismatch"
+    exit 1
+  fi
+
+  echo "golang unpacking $version"
+  tar -C $(dirname "$goroot") -xf "$tarball"
+fi
+
+cp "profile.sh" "/etc/profile.d/golang.sh"
+. profile.sh
