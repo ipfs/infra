371a33e6dd45d89a5e47c2d616be12be3ddf958f
Lars Gierth <larsg@systemli.org>
2017-02-08 04:33:43 +0100

    ssl: move ipfs.io cert to ipfs/gateway unit
    License: MIT
    Signed-off-by: Lars Gierth <larsg@systemli.org>

--- ipfs/gateway/build.sh
+++ ipfs/gateway/build.sh
@@ -4,3 +4,8 @@ set -e
 
 mkdir -p out/
 provsn_template nginx.conf out/6-ipfs-gateway.conf
+
+printf %s\\n "$(lookup ssl_cert)" > out/ipfs.io.crt
+printf %s\\n "$(lookup ssl_key)" > out/ipfs.io.key
+printf %s\\n "$(lookup ssl_trustchain)" > out/ipfs.io.trustchain.crt
+printf %s\\n "$(lookup ssl_dhparam)" > out/dhparam.pem
