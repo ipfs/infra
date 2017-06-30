03c79d99ec3266af7f9b1ed2006d78fcb000bca4
Lars Gierth <larsg@systemli.org>
2017-06-30 04:07:23 +0200

    prometheus: bump retention period to 5 years
    License: MIT
    Signed-off-by: Lars Gierth <larsg@systemli.org>

--- metrics/prometheus/env.sh
+++ metrics/prometheus/env.sh
@@ -4,7 +4,7 @@ all_prometheus_git="https://github.com/prometheus/prometheus.git"
 all_prometheus_ref="36fbdcc30fd13ad796381dc934742c559feeb1b5"
 
 all_prometheus_opts=("-storage.local.memory-chunks=512288"
-                     "-storage.local.retention=8760h0m0s"
+                     "-storage.local.retention=43800h0m0s"
                      "-web.external-url=http://metrics.ipfs.team/prometheus")
 
 all_prometheus_scrape_interval="15s"
