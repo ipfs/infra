83c2a19dd47e42b319bb6bfd30947feb5c1bc81f
Lars Gierth <larsg@systemli.org>
2018-04-06 05:36:43 +0200

    cleanup: remove the dead ansible code
    License: MIT
    Signed-off-by: Lars Gierth <larsg@systemli.org>

--- ansible/cleanup.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/sh
-
-for host in `ansible gateway --list-hosts`; do
-  echo "==> $host"
-  ssh root@$host.i.ipfs.io 'rm -v /var/lib/docker/containers/*/*-json.log ; docker restart ipfs ; sleep 2 ; docker rm $(docker ps -f "status=exited" -aq) ; docker rmi $(docker images -f "dangling=true" -aq)'
-done
