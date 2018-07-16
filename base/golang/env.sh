f84dcdbf3318fd33575094b13393415afaf5a7b7
Lars Gierth <larsg@systemli.org>
2018-07-16 15:43:27 -0400

    base: update golang to 1.10.3

--- base/golang/env.sh
+++ base/golang/env.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/env bash
 
-all_golang_version=go1.9.1
-all_golang_sha256sum=07d81c6b6b4c2dcf1b5ef7c27aaebd3691cdb40548500941f92b221147c5d9c7
+all_golang_version=go1.10.3
+all_golang_sha256sum=fa1b0e45d3b647c252f51f5e1204aba049cde4af177ef9f2181f43004f901035
 all_golang_goroot=/usr/local/go
